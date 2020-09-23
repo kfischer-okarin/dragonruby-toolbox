af02595b47dc294c3622e04b348198cfaaa27a43
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-09-23 14:41:31 +0900

    Refactor Tileset definitions

--- lib/autotile.rb
+++ lib/autotile.rb
@@ -15,18 +15,22 @@ module DRT
       [0, -1] => 0b00010000, [-1, 1] => 0b00100000, [-1, 0] => 0b01000000, [-1, 1] => 0b10000000
     }.freeze
 
-    def self.bitmask(*values)
-      case values[0]
-      when Fixnum
-        values[0]
-      when Array
-        values.map { |v| VECTORS[v] }.inject(0) { |sum, n| sum + n }
-      when Symbol
-        values.map { |v| SYMBOLS[v] }.inject(0) { |sum, n| sum + n }
-      else
-        raise "Value '#{values}' cannot be converted to bitmask"
+    module BitmaskHelper
+      def bitmask(*values)
+        case values[0]
+        when Fixnum
+          values[0]
+        when Array
+          values.map { |v| VECTORS[v] }.inject(0) { |sum, n| sum + n }
+        when Symbol
+          values.map { |v| SYMBOLS[v] }.inject(0) { |sum, n| sum + n }
+        else
+          raise "Value '#{values}' cannot be converted to bitmask"
+        end
       end
     end
+
+    extend BitmaskHelper
 
     # Bitmask conditions
     class Condition
@@ -241,98 +245,85 @@ module DRT
       end
     end
 
-    FULL_TILESET = (0...16).map { |row|
-      start = row * 16
-      (start...(start + 16)).to_a
-    }.reverse
+    module Tiles
+      extend BitmaskHelper
 
-    TILESET_47 = [
-      [
-        # 3x3 filled top row
-        bitmask(:left, :up, :right, :down_right, :down),
-        bitmask(:left, :down_left, :down, :down_right, :right),
-        bitmask(:right, :up, :left, :down_left, :down),
-        # 3x3 grid top row
-        bitmask(:right, :down),
-        bitmask(:left, :down, :right),
-        bitmask(:left, :down),
-        # vertical line top
-        bitmask(:down)
-      ],
-      [
-        # 3x3 filled middle row
-        bitmask(:up, :up_right, :right, :down_right, :down),
-        bitmask(:up, :up_right, :right, :down_right, :down, :down_left, :left, :up_left),
-        bitmask(:up, :up_left, :left, :down_left, :down),
-        # 3x3 grid middle row
-        bitmask(:right, :up, :down),
-        bitmask(:left, :right, :up, :down),
-        bitmask(:left, :up, :down),
-        #vertical line middle
-        bitmask(:up, :down)
-      ],
-      [
-        # 3x3 filled bottom row
-        bitmask(:left, :down, :right, :up_right, :up),
-        bitmask(:left, :up_left, :up, :up_right, :right),
-        bitmask(:right, :down, :left, :up_left, :up),
-        # 3x3 grid bottom row
-        bitmask(:right, :up),
-        bitmask(:left, :up, :right),
-        bitmask(:left, :up),
-        # vertical line bottm
-        bitmask(:up)
-      ],
-      [
-        # 3x3 block with thin lines extruding - top
-        bitmask(:right, :down_right, :down),
-        bitmask(:left, :up, :right, :down_right, :down, :down_left),
-        bitmask(:left, :down_left, :down),
-        # 2x2 Shuriken A - top
-        bitmask(:left, :down, :down_right, :right),
-        bitmask(:up, :left, :down_left, :down),
-        # 2x2 Shuriken B - top
-        bitmask(:up, :down, :down_right, :right),
-        bitmask(:right, :left, :down_left, :down)
+      CORNER_UP_LEFT = bitmask(:right, :down_right, :down)
+      CORNER_UP_RIGHT = bitmask(:left, :down_left, :down)
+      CORNER_DOWN_LEFT = bitmask(:right, :up_right, :up)
+      CORNER_DOWN_RIGHT = bitmask(:left, :up_left, :up)
 
-      ],
-      [
-        # 3x3 block with thin lines extruding - middle
-        bitmask(:up, :left, :down, :down_right, :right, :up_right),
-        nil,
-        bitmask(:up, :right, :down, :down_left, :left, :up_left),
-        # 2x2 Shuriken A - bottom
-        bitmask(:down, :right, :up_right, :up),
-        bitmask(:right, :up, :up_left, :left),
-        # 2x2 Shuriken B - bottom
-        bitmask(:left, :right, :up_right, :up),
-        bitmask(:down, :up, :up_left, :left)
-      ],
-      [
-        # 3x3 block with thin lines extruding - top
-        bitmask(:right, :up_right, :up),
-        bitmask(:left, :down, :right, :up_right, :up, :up_left),
-        bitmask(:left, :up_left, :up),
-        # Fat Plus - top
-        bitmask(:left, :up, :up_right, :right, :down_right, :down, :down_left),
-        bitmask(:right, :up, :up_left, :left, :down_left, :down, :down_right),
-        # Diagonal gaps
-        bitmask(:up, :up_right, :right, :down, :down_left, :left),
-        bitmask(:up, :up_left, :left, :down, :down_right, :right)
-      ],
-      [
-        # horizontal line
-        bitmask(:right),
-        bitmask(:left, :right),
-        bitmask(:left),
-        # Fat Plus - bottom
-        bitmask(:left, :down, :down_right, :right, :up_right, :up, :up_left),
-        bitmask(:right, :down, :down_left, :left, :up_left, :up, :up_right),
-        nil,
-        # Single element
-        0
+      SIDE_UP = bitmask(:left, :down_left, :down, :down_right, :right)
+      SIDE_DOWN = bitmask(:left, :up_left, :up, :up_right, :right)
+      SIDE_LEFT = bitmask(:up, :up_right, :right, :down_right, :down)
+      SIDE_RIGHT = bitmask(:up, :up_left, :left, :down_left, :down)
+
+      CENTER = bitmask(:up, :up_right, :right, :down_right, :down, :down_left, :left, :up_left)
+
+      CORNER_UP_LEFT_LINE_LEFT = bitmask(:left, :down, :down_right, :right)
+      CORNER_UP_LEFT_LINE_UP = bitmask(:up, :down, :down_right, :right)
+      CORNER_UP_RIGHT_LINE_UP = bitmask(:up, :left, :down_left, :down)
+      CORNER_UP_RIGHT_LINE_RIGHT = bitmask(:right, :left, :down_left, :down)
+      CORNER_DOWN_LEFT_LINE_DOWN = bitmask(:down, :right, :up_right, :up)
+      CORNER_DOWN_LEFT_LINE_LEFT = bitmask(:left, :right, :up_right, :up)
+      CORNER_DOWN_RIGHT_LINE_RIGHT = bitmask(:right, :up, :up_left, :left)
+      CORNER_DOWN_RIGHT_LINE_DOWN = bitmask(:down, :up, :up_left, :left)
+
+      CORNER_UP_LEFT_TWO_LINES = bitmask(:left, :up, :right, :down_right, :down)
+      CORNER_UP_RIGHT_TWO_LINES = bitmask(:right, :up, :left, :down_left, :down)
+      CORNER_DOWN_LEFT_TWO_LINES = bitmask(:left, :down, :right, :up_right, :up)
+      CORNER_DOWN_RIGHT_TWO_LINES = bitmask(:right, :down, :left, :up_left, :up)
+
+      SIDE_UP_LINE = bitmask(:left, :up, :right, :down_right, :down, :down_left)
+      SIDE_LEFT_LINE = bitmask(:up, :left, :down, :down_right, :right, :up_right)
+      SIDE_RIGHT_LINE = bitmask(:up, :right, :down, :down_left, :left, :up_left)
+      SIDE_DOWN_LINE = bitmask(:left, :down, :right, :up_right, :up, :up_left)
+
+      L_DOWN_RIGHT = bitmask(:right, :down)
+      L_DOWN_LEFT = bitmask(:left, :down)
+      L_UP_RIGHT = bitmask(:right, :up)
+      L_UP_LEFT = bitmask(:left, :up)
+
+      T_DOWN_LEFT_RIGHT = bitmask(:left, :down, :right)
+      T_UP_DOWN_RIGHT = bitmask(:right, :up, :down)
+      T_UP_DOWN_LEFT = bitmask(:left, :up, :down)
+      T_UP_LEFT_RIGHT = bitmask(:left, :up, :right)
+
+      PLUS = bitmask(:left, :right, :up, :down)
+
+      FAT_PLUS_UP_LEFT = bitmask(:left, :up, :up_right, :right, :down_right, :down, :down_left)
+      FAT_PLUS_UP_RIGHT = bitmask(:right, :up, :up_left, :left, :down_left, :down, :down_right)
+      FAT_PLUS_DOWN_LEFT = bitmask(:left, :down, :down_right, :right, :up_right, :up, :up_left)
+      FAT_PLUS_DOWN_RIGHT = bitmask(:right, :down, :down_left, :left, :up_left, :up, :up_right)
+
+      DIAGONAL_CONNECT_RIGHT = bitmask(:up, :up_right, :right, :down, :down_left, :left)
+      DIAGONAL_CONNECT_LEFT = bitmask(:up, :up_left, :left, :down, :down_right, :right)
+
+      VERTICAL_LINE_END_UP = bitmask(:down)
+      VERTICAL_LINE = bitmask(:up, :down)
+      VERTICAL_LINE_END_DOWN = bitmask(:up)
+
+      HORIZONTAL_LINE_END_LEFT = bitmask(:right)
+      HORIZONTAL_LINE = bitmask(:left, :right)
+      HORIZONTAL_LINE_END_RIGHT = bitmask(:left)
+
+      NO_NEIGHBORS = 0
+
+      TILESET_47 = [
+        [  CORNER_UP_LEFT_TWO_LINES,         SIDE_UP,   CORNER_UP_RIGHT_TWO_LINES,               L_DOWN_RIGHT,            T_DOWN_LEFT_RIGHT,                L_DOWN_LEFT,        VERTICAL_LINE_END_UP],
+        [                 SIDE_LEFT,          CENTER,                  SIDE_RIGHT,            T_UP_DOWN_RIGHT,                         PLUS,             T_UP_DOWN_LEFT,               VERTICAL_LINE],
+        [CORNER_DOWN_LEFT_TWO_LINES,       SIDE_DOWN, CORNER_DOWN_RIGHT_TWO_LINES,                 L_UP_RIGHT,              T_UP_LEFT_RIGHT,                  L_UP_LEFT,      VERTICAL_LINE_END_DOWN],
+        [            CORNER_UP_LEFT,    SIDE_UP_LINE,             CORNER_UP_RIGHT,   CORNER_UP_LEFT_LINE_LEFT,      CORNER_UP_RIGHT_LINE_UP,     CORNER_UP_LEFT_LINE_UP,  CORNER_UP_RIGHT_LINE_RIGHT],
+        [            SIDE_LEFT_LINE,             nil,             SIDE_RIGHT_LINE, CORNER_DOWN_LEFT_LINE_DOWN, CORNER_DOWN_RIGHT_LINE_RIGHT, CORNER_DOWN_LEFT_LINE_LEFT, CORNER_DOWN_RIGHT_LINE_DOWN],
+        [          CORNER_DOWN_LEFT,  SIDE_DOWN_LINE,           CORNER_DOWN_RIGHT,           FAT_PLUS_UP_LEFT,            FAT_PLUS_UP_RIGHT,      DIAGONAL_CONNECT_LEFT,      DIAGONAL_CONNECT_RIGHT],
+        [  HORIZONTAL_LINE_END_LEFT, HORIZONTAL_LINE,   HORIZONTAL_LINE_END_RIGHT,         FAT_PLUS_DOWN_LEFT,          FAT_PLUS_DOWN_RIGHT,                        nil,                NO_NEIGHBORS]
       ]
-    ].reverse
+
+      FULL_TILESET = (0...16).map { |row|
+        start = row * 16
+        (start...(start + 16)).to_a
+      }
+    end
 
     class TilesetBuilder
       def initialize(tile_size, tileset_definition)
@@ -342,7 +333,7 @@ module DRT
       end
 
       def build(source)
-        @tileset_definition.flat_map.with_index { |row, tile_y|
+        @tileset_definition.reverse.flat_map.with_index { |row, tile_y|
           row.map.with_index { |value, tile_x|
             next unless value
 
@@ -361,11 +352,11 @@ module DRT
     end
 
     def self.generate_full_tileset(autotile_source)
-      TilesetBuilder.new(32, FULL_TILESET).build(autotile_source)
+      TilesetBuilder.new(32, Tiles::FULL_TILESET).build(autotile_source)
     end
 
     def self.generate_tileset_47(autotile_source)
-      TilesetBuilder.new(32, TILESET_47).build(autotile_source)
+      TilesetBuilder.new(32, Tiles::TILESET_47).build(autotile_source)
     end
   end
 end
